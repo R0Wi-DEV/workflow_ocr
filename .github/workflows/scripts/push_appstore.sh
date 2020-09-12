17df70f9a5da6ff9da3554f02b7c4abf56c6bd29
Robin Windey <ro.windey@gmail.com>
2020-09-12 17:35:20 +0200

    Fix download path

--- .github/workflows/scripts/push_appstore.sh
+++ .github/workflows/scripts/push_appstore.sh
@@ -18,4 +18,4 @@ echo "Creating signature for app release"
 sign="`openssl dgst -sha512 -sign ${APP_NAME}.key ${APP_NAME}.tar.gz | openssl base64 -A`"
 
 echo "Creating new app release in Nextcloud appstore (nightly=${NIGHTLY})"
-curl -X POST https://apps.nextcloud.com/api/v1/apps/releases -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"$download_url\", \"signature\": \"$sign\", \"nightly\": ${NIGHTLY} }"
+curl -X POST https://apps.nextcloud.com/api/v1/apps/releases -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
