8e10d214a164c65b59c4e0505e3ad40f6d015f77
Robin Windey <ro.windey@gmail.com>
2020-09-14 12:23:52 +0200

    Remove encrypted PK and move to Github Secrets

--- .github/workflows/scripts/push_appstore.sh
+++ .github/workflows/scripts/push_appstore.sh
@@ -5,17 +5,13 @@
 #   APPSTORE_TOKEN
 #   NIGHTLY
 #   DOWNLOAD_URL
-#   ENCRYPTED_PK_PATH
-#   PK_DECRYPT_PW
+#   APP_PRIVATE_KEY_FILE
 
 echo "Downloading app tarball for signing"
 wget "$DOWNLOAD_URL" -O "${APP_NAME}.tar.gz"
 
-echo "Decrypt signing key"
-openssl aes-256-cbc -in $ENCRYPTED_PK_PATH -out ${APP_NAME}.key -pass pass:${PK_DECRYPT_PW} -d -iter 1000 -salt
-
 echo "Creating signature for app release"
-sign="`openssl dgst -sha512 -sign ${APP_NAME}.key ${APP_NAME}.tar.gz | openssl base64 -A`"
+sign="`openssl dgst -sha512 -sign ${APP_PRIVATE_KEY_FILE} ${APP_NAME}.tar.gz | openssl base64 -A`"
 
 echo "Creating new app release in Nextcloud appstore (nightly=${NIGHTLY})"
 curl -X POST https://apps.nextcloud.com/api/v1/apps/releases -H "Authorization: Token ${APPSTORE_TOKEN}" -H "Content-Type: application/json" -d "{\"download\":\"${DOWNLOAD_URL}\", \"signature\": \"${sign}\", \"nightly\": ${NIGHTLY} }"
