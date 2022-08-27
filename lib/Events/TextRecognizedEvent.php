ab3e0a65b207fa7b5dec6b46d73dcb107e8ac0d1
Guido Schmitz <g.schmitz@iurfriend.com>
2022-08-27 05:08:26 +0200

    Add TextRecognizedEvent class

---
+++ lib/Events/TextRecognizedEvent.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright Copyright (c) 2020 Robin Windey <ro.windey@gmail.com>
+ *
+ *  @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\WorkflowOcr\Events;
+
+use OCP\EventDispatcher\Event;
+use OCA\WorkflowOcr\OcrProcessors\OcrProcessorResult;
+
+/**
+ * Class TextRecognizedEvent
+ *
+ * @package OCA\WorkflowOcr\Events
+ */
+class TextRecognizedEvent extends Event {
+
+
+    /** @var OcrProcessorResult */
+    protected $result;
+
+
+    /**
+     * TextRecognizedEvent constructor.
+     *
+     * @param OcrProcessorResult $result
+     */
+    public function __construct(OcrProcessorResult $result) {
+        parent::__construct();
+        
+        $this->result = $result;
+    }
+
+    /**
+     * @return OcrProcessorResult $result
+     */
+    public function getResult(): OcrProcessorResult {
+        return $this->result;
+    }
+}
