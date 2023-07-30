3aebf0fa271dd5cf192a3488ea6a0a809a4171e5
Robin Windey <ro.windey@gmail.com>
2023-07-30 22:20:45 +0200

    Implement Notifications (#213)
    * Implement Notifications
    
    * Notification adjustments
    
    * Add documentation
    * Remove code smells
    * Update psalm-baseline to ignore IRootFolder dependency

---
+++ lib/Notification/Notifier.php
@@ -0,0 +1,127 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright Copyright (c) 2023 Robin Windey <ro.windey@gmail.com>
+ *
+ * @author Robin Windey <ro.windey@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\WorkflowOcr\Notification;
+
+use OCA\WorkflowOcr\AppInfo\Application;
+use OCP\Files\File;
+use OCP\Files\IRootFolder;
+use OCP\IURLGenerator;
+use OCP\L10N\IFactory;
+use OCP\Notification\AlreadyProcessedException;
+use OCP\Notification\INotification;
+use OCP\Notification\INotifier;
+use Psr\Log\LoggerInterface;
+
+class Notifier implements INotifier {
+	/** @var IFactory*/
+	private $l10nFactory;
+	/** @var IURLGenerator */
+	private $urlGenerator;
+	/** @var IRootFolder */
+	private $rootFolder;
+	/** @var LoggerInterface */
+	private $logger;
+
+	public function __construct(IFactory $factory,
+		IURLGenerator $urlGenerator,
+		IRootFolder $rootFolder,
+		LoggerInterface $logger) {
+		$this->l10nFactory = $factory;
+		$this->urlGenerator = $urlGenerator;
+		$this->rootFolder = $rootFolder;
+		$this->logger = $logger;
+	}
+
+	/**
+	 * Identifier of the notifier, only use [a-z0-9_]
+	 * @return string
+	 */
+	public function getID(): string {
+		return Application::APP_NAME;
+	}
+
+	/**
+	 * Human readable name describing the notifier
+	 * @return string
+	 */
+	public function getName(): string {
+		return $this->l10nFactory->get(Application::APP_NAME)->t('Workflow OCR');
+	}
+
+	/**
+	 * @param INotification $notification
+	 * @param string $languageCode The code of the language that should be used to prepare the notification
+	 */
+	public function prepare(INotification $notification, string $languageCode): INotification {
+		if ($notification->getApp() !== Application::APP_NAME) {
+			throw new \InvalidArgumentException();
+		}
+
+		$notification->setIcon($this->urlGenerator->imagePath(Application::APP_NAME, 'app-dark.svg'));
+		$l = $this->l10nFactory->get(Application::APP_NAME, $languageCode);
+
+		// Currently we only support sending notifications for ocr_error
+		if ($notification->getSubject() !== 'ocr_error') {
+			throw new \InvalidArgumentException();
+		}
+
+		$message = $notification->getSubjectParameters()['message'];
+		$notification
+			->setParsedSubject($l->t('Workflow OCR error'))
+			->setParsedMessage($message);
+		// Only add file info if we have one ...
+		if ($notification->getObjectType() === 'file' && $notification->getObjectId()) {
+			$richParams = $this->getRichParamForFile($notification);
+			$notification->setRichSubject($l->t('Workflow OCR error for file {file}'), $richParams);
+		}
+		return $notification;
+	}
+
+	private function getRichParamForFile(INotification $notification) : array {
+		try {
+			$userFolder = $this->rootFolder->getUserFolder($notification->getUser());
+			/** @var File[] */
+			$files = $userFolder->getById($notification->getObjectId());
+			/** @var File $file */
+			$file = array_shift($files);
+			$relativePath = $userFolder->getRelativePath($file->getPath());
+		} catch (\Throwable $th) {
+			$this->logger->error($th->getMessage(), ['exception' => $th]);
+			throw new AlreadyProcessedException();
+		}
+
+		return [
+			'file' => [
+				'type' => 'file',
+				'id' => $file->getId(),
+				'name' => $file->getName(),
+				'path' => $relativePath,
+				'link' => $this->urlGenerator->linkToRouteAbsolute('files.viewcontroller.showFile', ['fileid' => $notification->getObjectId()])
+			]
+		];
+	}
+}
---
+++ tests/Integration/Notification/NotificationTest.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * @copyright Copyright (c) 2023 Robin Windey <ro.windey@gmail.com>
+ *
+ *  @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\WorkflowOcr\Tests\Integration\Notification;
+
+use OC\BackgroundJob\JobList;
+use OCA\WorkflowOcr\BackgroundJobs\ProcessFileJob;
+use OCA\WorkflowOcr\Exception\OcrNotPossibleException;
+use OCA\WorkflowOcr\Helper\IProcessingFileAccessor;
+use OCA\WorkflowOcr\Service\IEventService;
+use OCA\WorkflowOcr\Service\INotificationService;
+use OCA\WorkflowOcr\Service\IOcrService;
+use OCA\WorkflowOcr\Service\NotificationService;
+use OCA\WorkflowOcr\Wrapper\IFilesystem;
+use OCA\WorkflowOcr\Wrapper\IViewFactory;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\DB\QueryBuilder\IExpressionBuilder;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\Files\File;
+use OCP\Files\FileInfo;
+use OCP\Files\IRootFolder;
+use OCP\IConfig;
+use OCP\IDBConnection;
+use OCP\IUser;
+use OCP\IUserManager;
+use OCP\IUserSession;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+class NotificationTest extends TestCase {
+	/** @var AppFake */
+	private $appFake;
+	/** @var LoggerInterface|MockObject */
+	private $logger;
+	/** @var IRootFolder|MockObject */
+	private $rootFolder;
+	/** @var IOcrService|MockObject */
+	private $ocrService;
+	/** @var IEventService|MockObject */
+	private $eventService;
+	/** @var IViewFactory|MockObject */
+	private $viewFactory;
+	/** @var IFilesystem|MockObject */
+	private $filesystem;
+	/** @var IUserSession|MockObject */
+	private $userSession;
+	/** @var IUserManager|MockObject */
+	private $userManager;
+	/** @var IUser|MockObject */
+	private $user;
+	/** @var IProcessingFileAccessor|MockObject */
+	private $processingFileAccessor;
+	/** @var INotificationService|MockObject */
+	private $notificationService;
+	/** @var JobList */
+	private $jobList;
+	/** @var ProcessFileJob */
+	private $processFileJob;
+
+	protected function setUp() : void {
+		parent::setUp();
+		// We use a faked notification receiver app to keep track of any notifications created
+		\OC::$server->get(\OCP\Notification\IManager::class)->registerApp(AppFake::class);
+		
+		// Use real Notification service to be able to check if notifications get created
+		$this->notificationService = new NotificationService(\OC::$server->get(\OCP\Notification\IManager::class));
+
+		$this->logger = $this->createMock(LoggerInterface::class);
+		$this->rootFolder = $this->createMock(IRootFolder::class);
+		$this->ocrService = $this->createMock(IOcrService::class);
+		$this->eventService = $this->createMock(IEventService::class);
+		$this->viewFactory = $this->createMock(IViewFactory::class);
+		$this->filesystem = $this->createMock(IFilesystem::class);
+		$this->userSession = $this->createMock(IUserSession::class);
+		$this->processingFileAccessor = $this->createMock(IProcessingFileAccessor::class);
+		
+		/** @var MockObject|IUserManager */
+		$userManager = $this->createMock(IUserManager::class);
+		$user = $this->createMock(IUser::class);
+		$userManager->method('get')
+			->withAnyParameters()
+			->willReturn($user);
+
+		$this->userManager = $userManager;
+		$this->user = $user;
+
+		$this->processFileJob = new ProcessFileJob(
+			$this->logger,
+			$this->rootFolder,
+			$this->ocrService,
+			$this->eventService,
+			$this->viewFactory,
+			$this->filesystem,
+			$this->userManager,
+			$this->userSession,
+			$this->processingFileAccessor,
+			$this->notificationService,
+			$this->createMock(ITimeFactory::class)
+		);
+		
+		/** @var IConfig */
+		$configMock = $this->createMock(IConfig::class);
+		/** @var ITimeFactory */
+		$timeFactoryMock = $this->createMock(ITimeFactory::class);
+		/** @var MockObject|IDbConnection */
+		$connectionMock = $this->createMock(IDBConnection::class);
+		/** @var MockObject|IQueryBuilder */
+		$queryBuilderMock = $this->createMock(IQueryBuilder::class);
+		$expressionBuilderMock = $this->createMock(IExpressionBuilder::class);
+
+		$queryBuilderMock->method('delete')
+			->withAnyParameters()
+			->willReturn($queryBuilderMock);
+		$queryBuilderMock->method('set')
+			->withAnyParameters()
+			->willReturn($queryBuilderMock);
+		$queryBuilderMock->method('update')
+			->withAnyParameters()
+			->willReturn($queryBuilderMock);
+		$queryBuilderMock->method('expr')
+			->withAnyParameters()
+			->willReturn($expressionBuilderMock);
+		$connectionMock->method('getQueryBuilder')
+			->withAnyParameters()
+			->willReturn($queryBuilderMock);
+
+		$this->jobList = new JobList(
+			$connectionMock,
+			$configMock,
+			$timeFactoryMock,
+			$this->logger
+		);
+
+		$this->processFileJob->setId(111);
+		$this->processFileJob->setArgument([
+			'filePath' => '/admin/files/somefile.pdf',
+			'uid' => 'someuser',
+			'settings' => '{}'
+		]);
+	}
+
+	public function testBackgroundJobCreatesErrorNotificationIfOcrFailed() {
+		$fileMock = $this->createValidFileMock();
+		$this->rootFolder->method('get')
+			->with('/admin/files/somefile.pdf')
+			->willReturn($fileMock);
+
+		$this->ocrService->expects($this->once())
+			->method('ocrFile')
+			->withAnyParameters()
+			->willThrowException(new OcrNotPossibleException('Some error'));
+		$appFake = \OC::$server->get(AppFake::class);
+
+		$this->processFileJob->start($this->jobList);
+
+		$notifications = $appFake->getNotifications();
+		$this->assertCount(1, $notifications);
+
+		$notification = $notifications[0];
+		$this->assertEquals('workflow_ocr', $notification->getApp());
+		$this->assertEquals('ocr_error', $notification->getSubject());
+		$this->assertEquals('OCR for file /admin/files/somefile.pdf not possible. Message: Some error', $notification->getSubjectParameters()['message']);
+	}
+
+	/**
+	 * @return File|MockObject
+	 */
+	private function createValidFileMock(string $mimeType = 'application/pdf', string $content = 'someFileContent', string $fileExtension = "pdf", string $path = "/admin/files/somefile.pdf") {
+		/** @var MockObject|File */
+		$fileMock = $this->createMock(File::class);
+		$fileMock->method('getType')
+			->willReturn(FileInfo::TYPE_FILE);
+		$fileMock->method('getMimeType')
+			->willReturn($mimeType);
+		$fileMock->method('getContent')
+			->willReturn($content);
+		$fileMock->method('getId')
+			->willReturn(42);
+		$fileMock->method('getExtension')
+			->willReturn($fileExtension);
+		$fileMock->method('getPath')
+			->willReturn($path);
+		return $fileMock;
+	}
+}
