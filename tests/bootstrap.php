9ecf21a64174f288edf4aaead5ef286b30913b33
Robin Windey <ro.windey@gmail.com>
2023-08-25 22:27:29 +0200

    NC28 compat

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,4 @@
 <?php
 
-use OCA\WorkflowOcr\AppInfo;
-
 require_once __DIR__ . '/../../../tests/bootstrap.php';
-
-\OC_App::loadApp(AppInfo\Application::APP_NAME);
-
-if (!class_exists('PHPUnit\Framework\TestCase')) {
-	require_once('PHPUnit/Autoload.php');
-}
-
-OC_Hook::clear();
+require_once __DIR__ . '/../composer/autoload.php';
